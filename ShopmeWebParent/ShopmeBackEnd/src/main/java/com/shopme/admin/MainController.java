32c4ea065c43aa9235e2a609a5cae7f66b7218d8
Hangyi Li <hal224@lehigh.edu>
2022-04-08 00:08:28 -0400

    set up multi-Module project

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -0,0 +1,16 @@
+package com.shopme.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MainController {
+	
+		@GetMapping("")                       //handle the HTTP GET request
+		public  String viewHomePage() {
+			return "index";
+		}
+
+}
