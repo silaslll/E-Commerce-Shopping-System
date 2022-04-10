e9fef1fec92fc6dce4136ecb12ede22e999ecd34
Hangyi Li <hal224@lehigh.edu>
2022-04-09 23:13:32 -0400

    create User page, add listing function

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -0,0 +1,53 @@
+package com.shopme.admin.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping("/users")
+	public String listAll(Model model) {
+		List<User> listUsers = service.listAll();
+		model.addAttribute("listUsers", listUsers);
+		
+		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
+		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		
+		return "redirect:/users";
+	}
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -0,0 +1,32 @@
+package com.shopme.admin.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+
+@Service								//@Service indicates that they're holding the business logic. Besides being used in the service layer, there isn't any other special use for this annotation.
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	public List<User> listAll() {
+		return (List<User>) userRepo.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		userRepo.save(user);
+	}
+}
