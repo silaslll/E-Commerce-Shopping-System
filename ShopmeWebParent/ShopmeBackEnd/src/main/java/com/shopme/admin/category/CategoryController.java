99bfe6848c845394c019c7d934809fb09e0ca4f3
Hangyi Li <hal224@lehigh.edu>
2022-04-20 17:13:15 -0400

    create Category Form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -21,4 +21,17 @@ public class CategoryController {
 
 		return "categories/categories";
 	}
+	
+
+	@GetMapping("/categories/new")
+	public String newCategory(Model model) {
+		List<Category> listCategories = service.listCategoriesUsedInForm();
+
+		model.addAttribute("category", new Category());
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("pageTitle", "Create New Category");
+
+		return "categories/category_form";
+	}
+	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -1,6 +1,8 @@
 package com.shopme.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,48 @@ import com.shopme.common.entity.Category;
 public class CategoryService {
 	@Autowired
 	private CategoryRepository repo;
-
+	
 	public List<Category> listAll() {
 		return (List<Category>) repo.findAll();
 	}
+	
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+		
+		Iterable<Category> categoriesInDB = repo.findAll();
+		
+		for (Category category : categoriesInDB) {
+			if (category.getParent() == null) {
+				categoriesUsedInForm.add(new Category(category.getName()));
+				
+				Set<Category> children = category.getChildren();
+				
+				for (Category subCategory : children) {
+					String name = "--" + subCategory.getName();
+					categoriesUsedInForm.add(new Category(name));
+					
+					listChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}		
+		
+		return categoriesUsedInForm;
+	}
+	
+	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+		
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {				
+				name += "--";
+			}
+			name += subCategory.getName();
+			
+			categoriesUsedInForm.add(new Category(name));
+			
+			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+		}		
+	}	
 }
