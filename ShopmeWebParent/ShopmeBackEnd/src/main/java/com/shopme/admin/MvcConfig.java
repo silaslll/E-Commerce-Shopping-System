1ef7775ee1906a6194a49e18282c50029e38ae6f
Hangyi Li <hal224@lehigh.edu>
2022-05-01 01:33:53 -0400

    Refactor MvcConfig

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -7,36 +7,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String dirName = "user-photos";
-		Path userPhotosDir = Paths.get(dirName);
+		exposeDirectory("user-photos", registry);
+		exposeDirectory("../category-images", registry);
+		exposeDirectory("../brand-logos", registry);		
+	}
+	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
 		
-		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/" + dirName + "/**")
-			.addResourceLocations("file:" + userPhotosPath + "/");    //delete / for mac os
-		
-
-		String categoryImagesDirName = "../category-images";
-		Path categoryImagesDir = Paths.get(categoryImagesDirName);
-
-		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-
-		registry.addResourceHandler("/category-images/**")
-			.addResourceLocations("file:" + categoryImagesPath + "/");
-
-		String brandLogosDirName = "../brand-logos";
-		Path brandLogosDir = Paths.get(brandLogosDirName);
-
-		String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
-
-		registry.addResourceHandler("/brand-logos/**")
-			.addResourceLocations("file:" + brandLogosPath + "/");		
+		String logicalPath = pathPattern.replace("../", "") + "/**";
+				
+		registry.addResourceHandler(logicalPath)
+			.addResourceLocations("file:" + absolutePath + "/");		
 	}
 
 }
