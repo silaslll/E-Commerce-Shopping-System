cea241731489ad5bbc74902d10d80c02ca2d28eb
Hangyi Li <hal224@lehigh.edu>
2022-05-05 09:52:22 -0400

    add check uniquess of product function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -33,4 +33,20 @@ public class ProductService {
 		
 		return repo.save(product);
 	}
+
+	public String checkUnique(Integer id, String name) {
+		boolean isCreatingNew = (id == null || id == 0);
+		Product productByName = repo.findByName(name);
+
+		if (isCreatingNew) {
+			if (productByName != null) return "Duplicate";
+		} else {
+			if (productByName != null && productByName.getId() != id) {
+				return "Duplicate";
+			}
+		}
+
+		return "OK";
+	}
+
 }
