d13dd57f3a9fd8d23b12cf552a1dac445668877b
Hangyi Li <hal224@lehigh.edu>
2022-05-08 11:59:50 -0400

    add ProductDetail entity class

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,7 +1,9 @@
 package com.shopme.common.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -71,6 +73,9 @@ public class Product {
 	
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private Set<ProductImage> images = new HashSet<>();
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<ProductDetail> details = new ArrayList<>();
 
 	public Integer getId() {
 		return id;
@@ -244,7 +249,19 @@ public class Product {
 	@Transient
 	public String getMainImagePath() {
 		if (id == null || mainImage == null) return "/images/image-thumbnail.png";
-
+		
 		return "/product-images/" + this.id + "/" + this.mainImage;
+	}
+
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
+	
+	public void addDetail(String name, String value) {
+		this.details.add(new ProductDetail(name, value, this));
 	}
 }
---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/ProductDetail.java
@@ -0,0 +1,70 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product_details")
+public class ProductDetail {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 255)
+	private String name;
+
+	@Column(nullable = false, length = 255)
+	private String value;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public ProductDetail() {
+	}
+
+	public ProductDetail(String name, String value, Product product) {
+		this.name = name;
+		this.value = value;
+		this.product = product;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+}
