c91d830e77821fbd73b2d98176a60da91778b958
Hangyi Li <hal224@lehigh.edu>
2022-05-09 12:08:10 -0400

    add details (specs) on product form

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
@@ -1,27 +1,17 @@
 var extraImagesCount = 0;
-dropdownBrands = $("#brand");
-dropdownCategories = $("#category");
 
-$(document).ready(function() {
-
-	$("#shortDescription").richText();
-	$("#fullDescription").richText();
-
-	dropdownBrands.change(function() {
-		dropdownCategories.empty();
-		getCategories();
-	});	
-
-	getCategories();
-
+$(document).ready(function() {	
 	$("input[name='extraImage']").each(function(index) {
 		extraImagesCount++;
-
+		
 		$(this).change(function() {
+			if (!checkFileSize(this)) {
+				return;
+			}			
 			showExtraImageThumbnail(this, index);
 		});
 	});
-
+	
 });
 
 function showExtraImageThumbnail(fileInput, index) {
@@ -30,9 +20,9 @@ function showExtraImageThumbnail(fileInput, index) {
 	reader.onload = function(e) {
 		$("#extraThumbnail" + index).attr("src", e.target.result);
 	};
-
+	
 	reader.readAsDataURL(file);	
-
+	
 	if (index >= extraImagesCount - 1) {
 		addNextExtraImageSection(index + 1);		
 	}
@@ -53,56 +43,20 @@ function addNextExtraImageSection(index) {
 			</div>
 		</div>	
 	`;
-
+	
 	htmlLinkRemove = `
 		<a class="btn fas fa-times-circle fa-2x icon-dark float-right"
 			href="javascript:removeExtraImage(${index - 1})" 
 			title="Remove this image"></a>
 	`;
-
+	
 	$("#divProductImages").append(htmlExtraImage);
-
+	
 	$("#extraImageHeader" + (index - 1)).append(htmlLinkRemove);
-
+	
 	extraImagesCount++;
 }
 
 function removeExtraImage(index) {
 	$("#divExtraImage" + index).remove();
 }
-
-function getCategories() {
-	brandId = dropdownBrands.val(); 
-	url = brandModuleURL + "/" + brandId + "/categories";
-
-	$.get(url, function(responseJson) {
-		$.each(responseJson, function(index, category) {
-			$("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
-		});			
-	});
-}
-
-function checkUnique(form) {
-	productId = $("#id").val();
-	productName = $("#name").val();
-
-	csrfValue = $("input[name='_csrf']").val();
-
-
-	params = {id: productId, name: productName, _csrf: csrfValue};
-
-	$.post(checkUniqueUrl, params, function(response) {
-		if (response == "OK") {
-			form.submit();
-		} else if (response == "Duplicate") {
-			showWarningModal("There is another product having the name " + productName);	
-		} else {
-			showErrorModal("Unknown response from server");
-		}
-
-	}).fail(function() {
-		showErrorModal("Could not connect to the server");
-	});
-
-	return false;
-}	 
