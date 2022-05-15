2bb8100acf64c45f496a86d1c522b4bd020d3d16
Hangyi Li <hal224@lehigh.edu>
2022-05-15 19:18:51 -0400

    add listing categories for home page

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
@@ -1,16 +1,25 @@
 package com.shopme;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+import com.shopme.category.CategoryService;
+import com.shopme.common.entity.Category;
 
 @Controller
 public class MainController {
 
+	@Autowired private CategoryService categoryService;
 	
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(Model model) {
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+		model.addAttribute("listCategories", listCategories);
+		
 		return "index";
 	}
 }
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/category/CategoryService.java
@@ -0,0 +1,31 @@
+package com.shopme.category;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Category;
+
+@Service
+public class CategoryService {
+
+	@Autowired private CategoryRepository repo;
+
+	public List<Category> listNoChildrenCategories() {
+		List<Category> listNoChildrenCategories = new ArrayList<>();
+
+		List<Category> listEnabledCategories = repo.findAllEnabled();
+
+		listEnabledCategories.forEach(category -> {
+			Set<Category> children = category.getChildren();
+			if (children == null || children.size() == 0) {
+				listNoChildrenCategories.add(category);
+			}
+		});
+
+		return listNoChildrenCategories;
+	}
+}
