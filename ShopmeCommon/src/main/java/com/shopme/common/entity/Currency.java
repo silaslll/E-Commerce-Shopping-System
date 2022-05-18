91e250c48ca1fb764a00efa980f40e3940840ee6
Hangyi Li <hal224@lehigh.edu>
2022-05-18 00:54:24 -0400

    add currency entity, repository and unit tests

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Currency.java
@@ -0,0 +1,69 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "currencies")
+public class Currency {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 64)
+	private String name;
+
+	@Column(nullable = false, length = 3)
+	private String symbol;
+
+	@Column(nullable = false, length = 4)
+	private String code;
+
+	public Currency() {
+
+	}
+
+	public Currency(String name, String symbol, String code) {
+		super();
+		this.name = name;
+		this.symbol = symbol;
+		this.code = code;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/CurrencyRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.setting;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Currency;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+
+}
