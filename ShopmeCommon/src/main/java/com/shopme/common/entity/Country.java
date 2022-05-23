08ceb92633c486abf20ae376ca349e644ca768db
Hangyi Li <hal224@lehigh.edu>
2022-05-22 23:31:30 -0400

    add controller layer for country

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -16,25 +16,36 @@ public class Country {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
+	
 	@Column(nullable = false, length = 45)
 	private String name;
-
+	
 	@Column(nullable = false, length = 5)
 	private String code;
-
+	
 	@OneToMany(mappedBy = "country")
 	private Set<State> states;
 
 	public Country() {
-
+		
 	}
+	
+	public Country(Integer id, String name, String code) {
+		this.id = id;
+		this.name = name;
+		this.code = code;
+	}
+
 
 	public Country(String name, String code) {
 		this.name = name;
 		this.code = code;
 	}
 
+	public Country(String name) {
+		this.name = name;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -59,18 +70,10 @@ public class Country {
 		this.code = code;
 	}
 
-	public Set<State> getStates() {
-		return states;
-	}
-
-	public void setStates(Set<State> states) {
-		this.states = states;
-	}
-
 	@Override
 	public String toString() {
 		return "Country [id=" + id + ", name=" + name + ", code=" + code + "]";
 	}
 
-
+	
 }
