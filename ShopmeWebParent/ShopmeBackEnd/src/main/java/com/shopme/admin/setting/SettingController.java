badd690a121d92099a9c40a8814e1dd7980ce45f
Hangyi Li <hal224@lehigh.edu>
2022-05-29 17:40:04 -0400

    add manage e-mail settings

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
@@ -24,35 +24,35 @@ import com.shopme.common.entity.Setting;
 public class SettingController {
 
 	@Autowired private SettingService service;
-
+	
 	@Autowired private CurrencyRepository currencyRepo;
-
+	
 	@GetMapping("/settings")
 	public String listAll(Model model) {
 		List<Setting> listSettings = service.listAllSettings();
 		List<Currency> listCurrencies = currencyRepo.findAllByOrderByNameAsc();
-
+		
 		model.addAttribute("listCurrencies", listCurrencies);
-
+		
 		for (Setting setting : listSettings) {
 			model.addAttribute(setting.getKey(), setting.getValue());
 		}
-
+		
 		return "settings/settings";
 	}
-
+	
 	@PostMapping("/settings/save_general")
 	public String saveGeneralSettings(@RequestParam("fileImage") MultipartFile multipartFile,
 			HttpServletRequest request, RedirectAttributes ra) throws IOException {
 		GeneralSettingBag settingBag = service.getGeneralSettings();
-
+		
 		saveSiteLogo(multipartFile, settingBag);
 		saveCurrencySymbol(request, settingBag);
-
+		
 		updateSettingValuesFromForm(request, settingBag.list());
-
+		
 		ra.addFlashAttribute("message", "General settings have been saved.");
-
+		
 		return "redirect:/settings";
 	}
 
@@ -61,23 +61,23 @@ public class SettingController {
 			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
 			String value = "/site-logo/" + fileName;
 			settingBag.updateSiteLogo(value);
-
+			
 			String uploadDir = "../site-logo/";
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 		}
 	}
-
+	
 	private void saveCurrencySymbol(HttpServletRequest request, GeneralSettingBag settingBag) {
 		Integer currencyId = Integer.parseInt(request.getParameter("CURRENCY_ID"));
 		Optional<Currency> findByIdResult = currencyRepo.findById(currencyId);
-
+		
 		if (findByIdResult.isPresent()) {
 			Currency currency = findByIdResult.get();
 			settingBag.updateCurrencySymbol(currency.getSymbol());
 		}
 	}
-
+	
 	private void updateSettingValuesFromForm(HttpServletRequest request, List<Setting> listSettings) {
 		for (Setting setting : listSettings) {
 			String value = request.getParameter(setting.getKey());
@@ -85,7 +85,27 @@ public class SettingController {
 				setting.setValue(value);
 			}
 		}
-
+		
 		service.saveAll(listSettings);
 	}
+	
+	@PostMapping("/settings/save_mail_server")
+	public String saveMailServerSetttings(HttpServletRequest request, RedirectAttributes ra) {
+		List<Setting> mailServerSettings = service.getMailServerSettings();
+		updateSettingValuesFromForm(request, mailServerSettings);
+		
+		ra.addFlashAttribute("message", "Mail server settings have been saved");
+		
+		return "redirect:/settings#mailServer";
+	}
+	
+	@PostMapping("/settings/save_mail_templates")
+	public String saveMailTemplateSetttings(HttpServletRequest request, RedirectAttributes ra) {
+		List<Setting> mailTemplateSettings = service.getMailTemplateSettings();
+		updateSettingValuesFromForm(request, mailTemplateSettings);
+		
+		ra.addFlashAttribute("message", "Mail template settings have been saved");
+		
+		return "redirect:/settings#mailTemplates";
+	}	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingService.java
@@ -12,24 +12,32 @@ import com.shopme.common.entity.SettingCategory;
 @Service
 public class SettingService {
 	@Autowired private SettingRepository repo;
-
+	
 	public List<Setting> listAllSettings() {
 		return (List<Setting>) repo.findAll();
 	}
-
+	
 	public GeneralSettingBag getGeneralSettings() {
 		List<Setting> settings = new ArrayList<>();
-
+		
 		List<Setting> generalSettings = repo.findByCategory(SettingCategory.GENERAL);
 		List<Setting> currencySettings = repo.findByCategory(SettingCategory.CURRENCY);
-
+		
 		settings.addAll(generalSettings);
 		settings.addAll(currencySettings);
-
+		
 		return new GeneralSettingBag(settings);
 	}
-
+	
 	public void saveAll(Iterable<Setting> settings) {
 		repo.saveAll(settings);
 	}
+	
+	public List<Setting> getMailServerSettings() {
+		return repo.findByCategory(SettingCategory.MAIL_SERVER);
+	}
+	
+	public List<Setting> getMailTemplateSettings() {
+		return repo.findByCategory(SettingCategory.MAIL_TEMPLATES);
+	}	
 }
