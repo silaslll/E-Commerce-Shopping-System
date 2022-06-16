bb64b26f0f59d7aacef9e4c0515323e20b390768
Hangyi Li <hal224@lehigh.edu>
2022-06-15 23:12:24 -0400

    add getEmailOfAuthenticatedCustomer function

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
@@ -5,30 +5,52 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+import com.shopme.security.oauth.CustomerOAuth2User;
 import com.shopme.setting.EmailSettingBag;
 
 public class Utility {
 	public static String getSiteURL(HttpServletRequest request) {
 		String siteURL = request.getRequestURL().toString();
-
+		
 		return siteURL.replace(request.getServletPath(), "");
 	}
-
+	
 	public static JavaMailSenderImpl prepareMailSender(EmailSettingBag settings) {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
+		
 		mailSender.setHost(settings.getHost());
 		mailSender.setPort(settings.getPort());
 		mailSender.setUsername(settings.getUsername());
 		mailSender.setPassword(settings.getPassword());
-
+		
 		Properties mailProperties = new Properties();
 		mailProperties.setProperty("mail.smtp.auth", settings.getSmtpAuth());
 		mailProperties.setProperty("mail.smtp.starttls.enable", settings.getSmtpSecured());
-
+		
 		mailSender.setJavaMailProperties(mailProperties);
-
+		
 		return mailSender;
 	}
+	
+	public static String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
+		Object principal = request.getUserPrincipal();
+		if (principal == null) return null;
+		
+		String customerEmail = null;
+		
+		if (principal instanceof UsernamePasswordAuthenticationToken 
+				|| principal instanceof RememberMeAuthenticationToken) {
+			customerEmail = request.getUserPrincipal().getName();
+		} else if (principal instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) principal;
+			CustomerOAuth2User oauth2User = (CustomerOAuth2User) oauth2Token.getPrincipal();
+			customerEmail = oauth2User.getEmail();
+		}
+		
+		return customerEmail;
+	}	
 }
