c02796d8e6f9f7e46d4b960e859e45dc1864fcb0
Hangyi Li <hal224@lehigh.edu>
2022-06-18 16:15:55 -0400

    add test

--- ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
@@ -87,4 +87,20 @@ public class AddressRepositoryTests {
 		Address address = repo.findByIdAndCustomer(addressId, customerId);
 		assertThat(address).isNull();
 	}	
+
+	@Test
+	public void testSetDefault() {
+		Integer addressId = 8;
+		repo.setDefaultAddress(addressId);
+
+		Address address = repo.findById(addressId).get();
+		assertThat(address.isDefaultForShipping()).isTrue();
+	}
+
+	@Test
+	public void testSetNonDefaultAddresses() {
+		Integer addressId = 8;
+		Integer customerId = 5;
+		repo.setNonDefaultForOthers(addressId, customerId);			
+	}
 }
