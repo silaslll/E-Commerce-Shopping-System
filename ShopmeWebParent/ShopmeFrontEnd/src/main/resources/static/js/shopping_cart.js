1748188a5ddc47484910cf004aa032ce950e3b21
Hangyi Li <hal224@lehigh.edu>
2022-06-18 00:04:20 -0400

    update frontend

--- ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
@@ -3,18 +3,23 @@ $(document).ready(function() {
 		evt.preventDefault();
 		decreaseQuantity($(this));
 	});
-
+	
 	$(".linkPlus").on("click", function(evt) {
 		evt.preventDefault();
 		increaseQuantity($(this));
-	});	
+	});
+	
+	$(".linkRemove").on("click", function(evt) {
+		evt.preventDefault();
+		removeProduct($(this));
+	});		
 });
 
 function decreaseQuantity(link) {
 	productId = link.attr("pid");
 	quantityInput = $("#quantity" + productId);
 	newQuantity = parseInt(quantityInput.val()) - 1;
-
+	
 	if (newQuantity > 0) {
 		quantityInput.val(newQuantity);
 		updateQuantity(productId, newQuantity);
@@ -27,7 +32,7 @@ function increaseQuantity(link) {
 		productId = link.attr("pid");
 		quantityInput = $("#quantity" + productId);
 		newQuantity = parseInt(quantityInput.val()) + 1;
-
+		
 		if (newQuantity <= 5) {
 			quantityInput.val(newQuantity);
 			updateQuantity(productId, newQuantity);
@@ -38,7 +43,7 @@ function increaseQuantity(link) {
 
 function updateQuantity(productId, quantity) {
 	url = contextPath + "cart/update/" + productId + "/" + quantity;
-
+	
 	$.ajax({
 		type: "POST",
 		url: url,
@@ -60,11 +65,56 @@ function updateSubtotal(updatedSubtotal, productId) {
 
 function updateTotal() {
 	total = 0.0;
-
+	productCount = 0;
+	
 	$(".subtotal").each(function(index, element) {
+		productCount++;
 		total += parseFloat(element.innerHTML.replaceAll(",", ""));
 	});
+	
+	if (productCount < 1) {
+		showEmptyShoppingCart();
+	} else {
+		formattedTotal = $.number(total, 2);
+		$("#total").text(formattedTotal);		
+	}
+	
+}
 
-	formattedTotal = $.number(total, 2);
-	$("#total").text(formattedTotal);
-} 
+function showEmptyShoppingCart() {
+	$("#sectionTotal").hide();
+	$("#sectionEmptyCartMessage").removeClass("d-none");
+}
+
+function removeProduct(link) {
+	url = link.attr("href");
+
+	$.ajax({
+		type: "DELETE",
+		url: url,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		}
+	}).done(function(response) {
+		rowNumber = link.attr("rowNumber");
+		removeProductHTML(rowNumber);
+		updateTotal();
+		updateCountNumbers();
+		
+		showModalDialog("Shopping Cart", response);
+		
+	}).fail(function() {
+		showErrorModal("Error while removing product.");
+	});				
+}
+
+function removeProductHTML(rowNumber) {
+	$("#row" + rowNumber).remove();
+	$("#blankLine" + rowNumber).remove();
+}
+
+function updateCountNumbers() {
+	$(".divCount").each(function(index, element) {
+		element.innerHTML = "" + (index + 1);
+	}); 
+}
