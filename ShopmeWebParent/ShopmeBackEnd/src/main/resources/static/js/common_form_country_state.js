0a2b28eeaea82c48690000f02b52a2fbd61f2bb7
Hangyi Li <hal224@lehigh.edu>
2022-06-18 01:19:06 -0400

    update frontend

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form_country_state.js
@@ -0,0 +1,31 @@
+var dropdownCountries;
+var dropdownStates;
+
+$(document).ready(function() {
+	dropdownCountries = $("#country");
+	dropdownStates = $("#listStates");
+
+	dropdownCountries.on("change", function() {
+		loadStates4Country();
+		$("#state").val("").focus();
+	});	
+
+	loadStates4Country();
+});
+
+function loadStates4Country() {
+	selectedCountry = $("#country option:selected");
+	countryId = selectedCountry.val();
+
+	url = contextPath + "states/list_by_country/" + countryId;
+
+	$.get(url, function(responseJson) {
+		dropdownStates.empty();
+
+		$.each(responseJson, function(index, state) {
+			$("<option>").val(state.name).text(state.name).appendTo(dropdownStates);
+		});
+	}).fail(function() {
+		showErrorModal("Error loading states/provinces for the selected country.");
+	})	
+}
