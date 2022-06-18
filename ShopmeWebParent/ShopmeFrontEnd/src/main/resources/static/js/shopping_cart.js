40b8dbb6fc8a15fa6f10f250736887f7df831c74
Hangyi Li <hal224@lehigh.edu>
2022-06-18 00:27:26 -0400

    format currency in shopping cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
@@ -1,3 +1,6 @@
+decimalSeparator = decimalPointType == 'COMMA' ? ',' : '.';
+thousandsSeparator = thousandsPointType == 'COMMA' ? ',' : '.'; 
+
 $(document).ready(function() {
 	$(".linkMinus").on("click", function(evt) {
 		evt.preventDefault();
@@ -59,8 +62,7 @@ function updateQuantity(productId, quantity) {
 }
 
 function updateSubtotal(updatedSubtotal, productId) {
-	formattedSubtotal = $.number(updatedSubtotal, 2);
-	$("#subtotal" + productId).text(formattedSubtotal);
+	$("#subtotal" + productId).text(formatCurrency(updatedSubtotal));
 }
 
 function updateTotal() {
@@ -69,14 +71,13 @@ function updateTotal() {
 	
 	$(".subtotal").each(function(index, element) {
 		productCount++;
-		total += parseFloat(element.innerHTML.replaceAll(",", ""));
+		total += parseFloat(clearCurrencyFormat(element.innerHTML));
 	});
 	
 	if (productCount < 1) {
 		showEmptyShoppingCart();
 	} else {
-		formattedTotal = $.number(total, 2);
-		$("#total").text(formattedTotal);		
+		$("#total").text(formatCurrency(total));		
 	}
 	
 }
@@ -117,4 +118,14 @@ function updateCountNumbers() {
 	$(".divCount").each(function(index, element) {
 		element.innerHTML = "" + (index + 1);
 	}); 
+}
+
+
+function formatCurrency(amount) {
+	return $.number(amount, decimalDigits, decimalSeparator, thousandsSeparator);
+}
+
+function clearCurrencyFormat(numberString) {
+	result = numberString.replaceAll(thousandsSeparator, "");
+	return result.replaceAll(decimalSeparator, ".");
 }
