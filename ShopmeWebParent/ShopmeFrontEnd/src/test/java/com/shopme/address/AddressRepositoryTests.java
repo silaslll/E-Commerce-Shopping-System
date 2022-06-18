42043327277a64e29b94d9f89cd8274236b4df32
Hangyi Li <hal224@lehigh.edu>
2022-06-18 15:45:32 -0400

    update test

--- ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
@@ -21,68 +21,69 @@ import com.shopme.common.entity.Customer;
 public class AddressRepositoryTests {
 
 	@Autowired private AddressRepository repo;
-
+	
 	@Test
 	public void testAddNew() {
 		Integer customerId = 5;
 		Integer countryId = 234; // USA
-
+		
 		Address newAddress = new Address();
 		newAddress.setCustomer(new Customer(customerId));
 		newAddress.setCountry(new Country(countryId));
-		newAddress.setFirstName("Charles");
-		newAddress.setLastName("Brugger");
-		newAddress.setPhoneNumber("646-232-3902");
-		newAddress.setAddressLine1("204 Morningview Lane");
-		newAddress.setCity("New York");
-		newAddress.setState("New York");
-		newAddress.setPostalCode("10013");
-
+		newAddress.setFirstName("Tobie");
+		newAddress.setLastName("Abel");
+		newAddress.setPhoneNumber("19094644165");
+		newAddress.setAddressLine1("4213 Gordon Street");
+		newAddress.setAddressLine2("Novak Building");
+		newAddress.setCity("Chino");
+		newAddress.setState("California");
+		newAddress.setPostalCode("91710");
+		
 		Address savedAddress = repo.save(newAddress);
-
+		
 		assertThat(savedAddress).isNotNull();
 		assertThat(savedAddress.getId()).isGreaterThan(0);
 	}
-
+	
 	@Test
 	public void testFindByCustomer() {
 		Integer customerId = 5;
 		List<Address> listAddresses = repo.findByCustomer(new Customer(customerId));
 		assertThat(listAddresses.size()).isGreaterThan(0);
-
+		
 		listAddresses.forEach(System.out::println);
 	}
-
+	
 	@Test
 	public void testFindByIdAndCustomer() {
 		Integer addressId = 1;
 		Integer customerId = 5;
-
+		
 		Address address = repo.findByIdAndCustomer(addressId, customerId);
-
+		
 		assertThat(address).isNotNull();
 		System.out.println(address);
 	}
-
+	
 	@Test
 	public void testUpdate() {
 		Integer addressId = 1;
 		String phoneNumber = "646-232-3932";
-
+		
 		Address address = repo.findById(addressId).get();
 		address.setPhoneNumber(phoneNumber);
 
 		Address updatedAddress = repo.save(address);
 		assertThat(updatedAddress.getPhoneNumber()).isEqualTo(phoneNumber);
 	}
-
+	
 	@Test
 	public void testDeleteByIdAndCustomer() {
 		Integer addressId = 1;
 		Integer customerId = 5;
-
+		
 		repo.deleteByIdAndCustomer(addressId, customerId);
-
+		
 		Address address = repo.findByIdAndCustomer(addressId, customerId);
 		assertThat(address).isNull();
 	}	
