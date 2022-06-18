78b913b7399635f24f9dcef7115687e2b42de7a1
Hangyi Li <hal224@lehigh.edu>
2022-06-18 17:00:30 -0400

    update frontend

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/order/OrderRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/order/OrderRepositoryTests.java
@@ -27,28 +27,28 @@ public class OrderRepositoryTests {
 
 	@Autowired private OrderRepository repo;
 	@Autowired private TestEntityManager entityManager;
-
+	
 	@Test
 	public void testCreateNewOrderWithSingleProduct() {
 		Customer customer = entityManager.find(Customer.class, 1);
 		Product product = entityManager.find(Product.class, 1);
-
+		
 		Order mainOrder = new Order();
 		mainOrder.setOrderTime(new Date());
 		mainOrder.setCustomer(customer);
 		mainOrder.copyAddressFromCustomer();
-
+		
 		mainOrder.setShippingCost(10);
 		mainOrder.setProductCost(product.getCost());
 		mainOrder.setTax(0);
 		mainOrder.setSubtotal(product.getPrice());
 		mainOrder.setTotal(product.getPrice() + 10);
-
+		
 		mainOrder.setPaymentMethod(PaymentMethod.CREDIT_CARD);
 		mainOrder.setStatus(OrderStatus.NEW);
 		mainOrder.setDeliverDate(new Date());
 		mainOrder.setDeliverDays(1);
-
+		
 		OrderDetail orderDetail = new OrderDetail();
 		orderDetail.setProduct(product);
 		orderDetail.setOrder(mainOrder);
@@ -57,25 +57,25 @@ public class OrderRepositoryTests {
 		orderDetail.setQuantity(1);
 		orderDetail.setSubtotal(product.getPrice());
 		orderDetail.setUnitPrice(product.getPrice());
-
+		
 		mainOrder.getOrderDetails().add(orderDetail);
-
+		
 		Order savedOrder = repo.save(mainOrder);
-
+		
 		assertThat(savedOrder.getId()).isGreaterThan(0);		
 	}
-
+	
 	@Test
 	public void testCreateNewOrderWithMultipleProducts() {
-		Customer customer = entityManager.find(Customer.class, 2);
-		Product product1 = entityManager.find(Product.class, 3);
-		Product product2 = entityManager.find(Product.class, 5);
-
+		Customer customer = entityManager.find(Customer.class, 10);
+		Product product1 = entityManager.find(Product.class, 20);
+		Product product2 = entityManager.find(Product.class, 40);
+		
 		Order mainOrder = new Order();
 		mainOrder.setOrderTime(new Date());
 		mainOrder.setCustomer(customer);
 		mainOrder.copyAddressFromCustomer();
-
+		
 		OrderDetail orderDetail1 = new OrderDetail();
 		orderDetail1.setProduct(product1);
 		orderDetail1.setOrder(mainOrder);
@@ -84,7 +84,7 @@ public class OrderRepositoryTests {
 		orderDetail1.setQuantity(1);
 		orderDetail1.setSubtotal(product1.getPrice());
 		orderDetail1.setUnitPrice(product1.getPrice());
-
+		
 		OrderDetail orderDetail2 = new OrderDetail();
 		orderDetail2.setProduct(product2);
 		orderDetail2.setOrder(mainOrder);
@@ -93,64 +93,64 @@ public class OrderRepositoryTests {
 		orderDetail2.setQuantity(2);
 		orderDetail2.setSubtotal(product2.getPrice() * 2);
 		orderDetail2.setUnitPrice(product2.getPrice());
-
+		
 		mainOrder.getOrderDetails().add(orderDetail1);
 		mainOrder.getOrderDetails().add(orderDetail2);
-
+		
 		mainOrder.setShippingCost(30);
 		mainOrder.setProductCost(product1.getCost() + product2.getCost());
 		mainOrder.setTax(0);
 		float subtotal = product1.getPrice() + product2.getPrice() * 2;
 		mainOrder.setSubtotal(subtotal);
 		mainOrder.setTotal(subtotal + 30);
-
-		mainOrder.setPaymentMethod(PaymentMethod.COD);
-		mainOrder.setStatus(OrderStatus.PROCESSING);
+		
+		mainOrder.setPaymentMethod(PaymentMethod.CREDIT_CARD);
+		mainOrder.setStatus(OrderStatus.PACKAGED);
 		mainOrder.setDeliverDate(new Date());
 		mainOrder.setDeliverDays(3);
-
+		
 		Order savedOrder = repo.save(mainOrder);		
 		assertThat(savedOrder.getId()).isGreaterThan(0);		
 	}
-
+	
 	@Test
 	public void testListOrders() {
 		Iterable<Order> orders = repo.findAll();
-
+		
 		assertThat(orders).hasSizeGreaterThan(0);
-
+		
 		orders.forEach(System.out::println);
 	}
-
+	
 	@Test
 	public void testUpdateOrder() {
 		Integer orderId = 2;
 		Order order = repo.findById(orderId).get();
-
+		
 		order.setStatus(OrderStatus.SHIPPING);
 		order.setPaymentMethod(PaymentMethod.COD);
 		order.setOrderTime(new Date());
 		order.setDeliverDays(2);
-
+		
 		Order updatedOrder = repo.save(order);
-
+		
 		assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.SHIPPING);
 	}
-
+	
 	@Test
 	public void testGetOrder() {
 		Integer orderId = 3;
 		Order order = repo.findById(orderId).get();
-
+		
 		assertThat(order).isNotNull();
 		System.out.println(order);
 	}
-
+	
 	@Test
 	public void testDeleteOrder() {
 		Integer orderId = 3;
 		repo.deleteById(orderId);
-
+		
 		Optional<Order> result = repo.findById(orderId);
 		assertThat(result).isNotPresent();
 	}
