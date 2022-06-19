4233110fe4b2d379c37ce1221b1f82deacf19227
Hangyi Li <hal224@lehigh.edu>
2022-06-19 17:12:12 -0400

    code refactor

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderService.java
@@ -73,6 +73,14 @@ public class OrderService {
 			orderDetails.add(orderDetail);
 		}
 		
+		OrderTrack track = new OrderTrack();
+		track.setOrder(newOrder);
+		track.setStatus(OrderStatus.NEW);
+		track.setNotes(OrderStatus.NEW.defaultDescription());
+		track.setUpdatedTime(new Date());
+		
+		newOrder.getOrderTracks().add(track);
+		
 		return repo.save(newOrder);
 	}
 	
