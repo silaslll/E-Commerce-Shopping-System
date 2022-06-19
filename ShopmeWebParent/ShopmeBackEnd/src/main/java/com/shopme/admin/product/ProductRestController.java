5b359f4c988229d26b20607b6ea7a49b244decb3
Hangyi Li <hal224@lehigh.edu>
2022-06-19 14:49:52 -0400

    Code add products to an order

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRestController.java
@@ -1,8 +1,13 @@
 package com.shopme.admin.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 
 @RestController
 public class ProductRestController {
@@ -13,4 +18,12 @@ public class ProductRestController {
 	public String checkUnique(Integer id, String name) {
 		return service.checkUnique(id, name);
 	}	
+	
+	@GetMapping("/products/get/{id}")
+	public ProductDTO getProductInfo(@PathVariable("id") Integer id) 
+			throws ProductNotFoundException {
+		Product product = service.get(id);
+		return new ProductDTO(product.getName(), product.getMainImagePath(), 
+				product.getDiscountPrice(), product.getCost());
+	}
 }
