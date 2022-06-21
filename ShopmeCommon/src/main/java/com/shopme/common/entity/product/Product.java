f81a5d503e77fb26be7b67751b6cae9b8b43fe57
Hangyi Li <hal224@lehigh.edu>
2022-06-20 20:56:16 -0400

    issue

--- ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
@@ -25,10 +25,10 @@ import com.shopme.common.Constants;
 @Table(name = "products")
 public class Product extends IdBasedEntity {
 	
-	@Column(unique = true, length = 256, nullable = false)
+	@Column(unique = true, length = 255, nullable = false)
 	private String name;
 	
-	@Column(unique = true, length = 256, nullable = false)
+	@Column(unique = true, length = 255, nullable = false)
 	private String alias;
 	
 	@Column(length = 512, nullable = false, name = "short_description")
