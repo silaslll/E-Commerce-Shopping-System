58bbdca55c3198dbfe696eb4c208421dc2829b0d
Hangyi Li <hal224@lehigh.edu>
2022-07-04 21:59:31 -0400

    add to order

--- ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
@@ -60,6 +60,17 @@ public class Order extends AbstractAddress {
 	@OrderBy("updatedTime ASC")
 	private List<OrderTrack> orderTracks = new ArrayList<>();
 
+	public Order() {
+	}
+
+	public Order(Integer id, Date orderTime, float productCost, float subtotal, float total) {
+		this.id = id;
+		this.orderTime = orderTime;
+		this.productCost = productCost;
+		this.subtotal = subtotal;
+		this.total = total;
+	}
+	
 	public String getCountry() {
 		return country;
 	}
