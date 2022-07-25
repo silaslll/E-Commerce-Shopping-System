d555428b7cf4448ad6ecb061eff2d0d82f15fa31
Hangyi Li <hal224@lehigh.edu>
2022-07-25 01:53:36 -0400

    add restController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportRestController.java
@@ -10,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReportRestController {
 	@Autowired private MasterOrderReportService masterOrderReportService;
-
+	
 	@GetMapping("/reports/sales_by_date/{period}")
 	public List<ReportItem> getReportDataByDatePeriod(@PathVariable("period") String period) {
 		System.out.println("Report period: " + period);
-		return masterOrderReportService.getReportDataLast7Days();
+		
+		switch (period) {
+			case "last_7_days":
+				return masterOrderReportService.getReportDataLast7Days();
+				
+			case "last_28_days":
+				return masterOrderReportService.getReportDataLast28Days();
+			default:
+				return masterOrderReportService.getReportDataLast7Days();
+		}
+		
 	}
 }
