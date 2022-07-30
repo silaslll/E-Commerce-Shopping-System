810be454ee57ef613c0f52821c56d6042570aa9e
Hangyi Li <hal224@lehigh.edu>
2022-07-30 15:08:43 -0400

    add to report

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
@@ -35,16 +35,16 @@ public class MasterOrderReportService {
 		System.out.println("Start time: " + startTime);
 		System.out.println("End time: " + endTime);
 		
-		dateFormatter = new SimpleDateFormat("yyy-MM-dd");
+		dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 		
-		return getReportDataByDateRange(startTime, endTime);
+		return getReportDataByDateRange(startTime, endTime, "days");
 	}
 	
-	private List<ReportItem> getReportDataByDateRange(Date startTime, Date endTime) {
+	private List<ReportItem> getReportDataByDateRange(Date startTime, Date endTime, String period) {
 		List<Order> listOrders = repo.findByOrderTimeBetween(startTime, endTime);
 		printRawData(listOrders);
 		
-		List<ReportItem> listReportItems = createReportData(startTime, endTime);
+		List<ReportItem> listReportItems = createReportData(startTime, endTime, period);
 		
 		System.out.println();
 		
@@ -81,7 +81,7 @@ public class MasterOrderReportService {
 		
 	}
 
-	private List<ReportItem> createReportData(Date startTime, Date endTime) {
+	private List<ReportItem> createReportData(Date startTime, Date endTime, String period) {
 		List<ReportItem> listReportItems = new ArrayList<>();
 		
 		Calendar startDate = Calendar.getInstance();
@@ -96,7 +96,12 @@ public class MasterOrderReportService {
 		listReportItems.add(new ReportItem(dateString));
 		
 		do {
-			startDate.add(Calendar.DAY_OF_MONTH, 1);
+			if (period.equals("days")) {
+				startDate.add(Calendar.DAY_OF_MONTH, 1);
+			} else if (period.equals("months")) {
+				startDate.add(Calendar.MONTH, 1);
+			}
+			
 			currentDate = startDate.getTime();
 			dateString = dateFormatter.format(currentDate);	
 			
@@ -113,4 +118,26 @@ public class MasterOrderReportService {
 					order.getId(), order.getOrderTime(), order.getTotal(), order.getProductCost());
 		});
 	}
+
+	public List<ReportItem> getReportDataLast6Months() {
+		return getReportDataLastXMonths(6);
+	}
+
+	public List<ReportItem> getReportDataLastYear() {
+		return getReportDataLastXMonths(12);
+	}
+	
+	private List<ReportItem> getReportDataLastXMonths(int months) {
+		Date endTime = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH, -(months - 1));
+		Date startTime = cal.getTime();
+		
+		System.out.println("Start time: " + startTime);
+		System.out.println("End time: " + endTime);
+		
+		dateFormatter = new SimpleDateFormat("yyyy-MM");
+		
+		return getReportDataByDateRange(startTime, endTime, "months");
+	}	
 }
