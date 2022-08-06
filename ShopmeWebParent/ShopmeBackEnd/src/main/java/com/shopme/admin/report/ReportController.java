a73c23e65fa87e1baf1ddd7952dc6bfb651314f3
Hangyi Li <hal224@lehigh.edu>
2022-08-06 09:52:32 -0400

    add to report controller

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
@@ -1,13 +1,50 @@
 package com.shopme.admin.report;
 
-import org.springframework.stereotype.Controller;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class ReportController {
+@RestController
+public class ReportRestController {
+	@Autowired private MasterOrderReportService masterOrderReportService;
+	
+	@GetMapping("/reports/sales_by_date/{period}")
+	public List<ReportItem> getReportDataByDatePeriod(@PathVariable("period") String period) {
+		System.out.println("Report period: " + period);
+		
+		switch (period) {
+			case "last_7_days":
+				return masterOrderReportService.getReportDataLast7Days();
+				
+			case "last_28_days":
+				return masterOrderReportService.getReportDataLast28Days();
 
-	@GetMapping("/reports")
-	public String viewSalesReportHome() {
-		return "reports/reports";
+			case "last_6_months":
+				return masterOrderReportService.getReportDataLast6Months();
+
+			case "last_year":
+				return masterOrderReportService.getReportDataLastYear();
+				
+			default:
+				return masterOrderReportService.getReportDataLast7Days();
+		}
+		
+	}
+	
+	@GetMapping("/reports/sales_by_date/{startDate}/{endDate}")
+	public List<ReportItem> getReportDataByDatePeriod(@PathVariable("startDate") String startDate,
+			@PathVariable("endDate") String endDate) throws ParseException {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date startTime = dateFormatter.parse(startDate);
+		Date endTime = dateFormatter.parse(endDate);
+		
+		return masterOrderReportService.getReportDataByDateRange(startTime, endTime);
 	}
 }
