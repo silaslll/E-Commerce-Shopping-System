8f91b52a23a164b65694765b8a9ea9aa527d65c1
Hangyi Li <hal224@lehigh.edu>
2022-08-10 02:16:15 -0400

    add test

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/report/ReportRestControllerTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/report/ReportRestControllerTests.java
@@ -32,4 +32,15 @@ public class ReportRestControllerTests {
 
 		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());
 	}	
+	
+	@Test
+	@WithMockUser(username = "user1", password = "pass1", authorities = {"Salesperson"})
+	public void testGetReportDataByDateRange() throws Exception {
+		String startDate = "2021-09-01";
+		String endDate = "2021-09-30";
+		String requestURL = "/reports/sales_by_date/" + startDate + "/" + endDate;
+
+
+		mockMvc.perform(get(requestURL)).andExpect(status().isOk()).andDo(print());
+	}		
 }
