45744521a822631aa8399bf943532795b2bd669b
Hangyi Li <hal224@lehigh.edu>
2022-08-11 15:07:41 -0400

    format concurrency

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
@@ -1,13 +1,32 @@
 package com.shopme.admin.report;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shopme.admin.setting.SettingService;
+import com.shopme.common.entity.setting.Setting;
+
 @Controller
 public class ReportController {
-
+	
+	@Autowired private SettingService settingService;
+	
 	@GetMapping("/reports")
-	public String viewSalesReportHome() {
+	public String viewSalesReportHome(HttpServletRequest request) {
+		loadCurrencySetting(request);
 		return "reports/reports";
 	}
+	
+	private void loadCurrencySetting(HttpServletRequest request) {
+		List<Setting> currencySettings = settingService.getCurrencySettings();
+		
+		for (Setting setting : currencySettings) {
+			request.setAttribute(setting.getKey(), setting.getValue());
+		}	
+	}	
 }
